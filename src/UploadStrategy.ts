1df07777942e240fc99ba9b961b72a30830bf575
inobit <inoechoc@gmail.com>
2026-04-01 11:29:00 +0800

    chore: 移除 Imgur 相关代码和测试

--- src/UploadStrategy.ts
+++
@@ -1,6 +0,0 @@
-export const UploadStrategies = {
-  ANONYMOUS_IMGUR: 'Anonymous Imgur upload',
-  AUTHENTICATED_IMGUR: 'Authenticated Imgur upload',
-} as const
-
-export type UploadStrategy = keyof typeof UploadStrategies
